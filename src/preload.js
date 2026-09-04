8486b73c1000689375fb7306a845b399b9fa6de4
OxygenForge-code <oxygenforge@gmail.com>
2026-09-04 16:22:00 +0000

    feat: expose download and loading events to browser UI

--- src/preload.js
+++ src/preload.js
@@ -1,4 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
+
 contextBridge.exposeInMainWorld('geniskapi', {
   navigate: value => ipcRenderer.invoke('navigate', value),
   back: () => ipcRenderer.invoke('back'),
@@ -6,5 +7,9 @@ contextBridge.exposeInMainWorld('geniskapi', {
   reload: () => ipcRenderer.invoke('reload'),
   home: () => ipcRenderer.invoke('home'),
   newWindow: () => ipcRenderer.invoke('new-window'),
-  onUrl: cb => ipcRenderer.on('page-url', (_, url) => cb(url))
+  onUrl: cb => ipcRenderer.on('page-url', (_, url) => cb(url)),
+  onLoading: cb => ipcRenderer.on('page-loading', (_, loading) => cb(Boolean(loading))),
+  onDownloadStarted: cb => ipcRenderer.on('download-started', (_, data) => cb(data)),
+  onDownloadProgress: cb => ipcRenderer.on('download-progress', (_, data) => cb(data)),
+  onDownloadFinished: cb => ipcRenderer.on('download-finished', (_, data) => cb(data))
 });
